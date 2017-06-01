6ca2585662c42cc08150a6b359913f52cb59294d
eitanfrailich <eitan.fr5@gmail.com>
2017-06-01 17:55:14 +0300

    add workbox config

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{png,xml,svg,ico,eot,woff2,ttf,woff,html,js,json,css}"
+  ],
+  "swDest": "dist/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
